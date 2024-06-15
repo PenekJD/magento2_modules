5d8ab37e6db02e3e10a169fd50c7b7d536834bf4
DzmitryiHrynchyk <grinchikdmitry@gmail.com>
2024-06-15 14:34:00 +0200

    Added dataPatch for product label

---
+++ Penekjd/DataPatch/Setup/Patch/Data/AddProductLabelAttribute.php
@@ -0,0 +1,51 @@
+<?php
+namespace Penekjd\DataPatch\Setup\Patch\Data;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+class AddProductLabelAttribute implements DataPatchInterface
+{
+    public function __construct(
+        private ModuleDataSetupInterface $moduleDataSetup,
+        private EavSetupFactory $eavSetupFactory
+    ) {
+    }
+
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'label_text',
+            [
+                'type' => 'varchar',
+                'label' => 'Label Text',
+                'input' => 'text',
+                'required' => false,
+                'sort_order' => 100,
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'user_defined' => true,
+                'default' => '',
+                'group' => 'General',
+            ]
+        );
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
